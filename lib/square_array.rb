b025f3eed6bf074ba7c5b6ad360d54b54f030d7c
Jeffrey Platt <jsplatt@gmail.com>
2020-11-30 15:26:22 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,9 +1,19 @@
-def square_array(array)
+#def square_array(array)
   
-  new_array = []
+  #new_array = []
 
-array.each do |integer|
-  new_array << integer ** 2
- end
- return new_array
+#array.each do |integer|
+  #new_array << integer ** 2
+ #end
+ #return new_array
+#end
+
+def square_array(array)
+  new_array = []
+  counter = 0
+  while new_array[counter] do
+    puts array[counter] ** 2
+    counter += 1
+  end
+  new_array
 end
